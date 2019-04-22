165dadae04a05f1fad8b69d96917d4c9bf870d86
Rasarts <rasart.pro@gmail.com>
2019-04-22 07:16:36 +0300

    Adding tests.

--- test/notification_center_test.dart
+++ test/notification_center_test.dart
@@ -1,9 +1,12 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Notification;
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:notification_center/blocs.dart' show NotificationCenterBloc;
+import 'package:notification_center/models.dart';
 import 'package:notification_center/notification_center.dart'
     show NotificationCenterWidget;
+
+import 'package:notification_center/models.dart' show NotificationModel;
 
 void main() {
   group('Notification center', () {
@@ -30,9 +33,53 @@ void main() {
       expect(find.text(testText), findsOneWidget);
     });
 
-    test('can show notification with other notifications', () {});
+    testWidgets('can show notification', (tester) async {
+      Widget notificationCenter;
+
+      notificationCenter = NotificationCenterWidget(
+          notificationCenterBloc: notificationCenterBloc, child: Offstage());
+
+      await tester.pumpWidget(MaterialApp(home: notificationCenter));
+
+      final notification = NotificationModel(
+          header: NotificationHeaderModel(text: 'Header'),
+          body: NotificationBodyModel(text: 'Body'));
+
+      notificationCenterBloc.notifications.add(notification);
+
+      await tester.runAsync(() => tester.pump());
+      await tester.pump();
+      await tester.pumpAndSettle(Duration(seconds: 3));
+      await tester.idle();
+
+      expect(find.text('Header'), findsOneWidget);
+      expect(find.text('Body'), findsOneWidget);
+    });
+
+    test('can show notification with other notifications', () {
+      final firstNotification = NotificationModel(
+          showWithNotificationsFromHistory: true,
+          decoration: BoxDecoration(
+              color: Color(0xFFfecfbf),
+              borderRadius: BorderRadius.circular(6),
+              border: Border.all(color: Color(0xFFb7315c))),
+          margin: EdgeInsets.all(3),
+          padding: EdgeInsets.all(2),
+          header: NotificationHeaderModel(
+              padding: EdgeInsets.only(top: 10, left: 10, bottom: 2, right: 10),
+              text: 'Notification header',
+              textStyle: TextStyle(
+                  color: Color(0xFFb7315c),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold)),
+          body: NotificationBodyModel(
+              padding: EdgeInsets.only(top: 5, left: 10, bottom: 10, right: 10),
+              textStyle: TextStyle(color: Color(0xFFb7315c)),
+              text: 'Notification body'));
+    });
 
     test('can show all notifications', () {});
+
     test('can hide all notifications', () {});
   });
 }
