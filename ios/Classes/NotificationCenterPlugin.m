f7f2271e7ebdeb1cca5403473725e827a08a6415
Vitaliy <vostrikov.vitaliy.mikhailovich@gmail.com>
2021-03-26 13:27:34 +0300

    Cleanup

--- ios/Classes/NotificationCenterPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "NotificationCenterPlugin.h"
-#import <notification_center/notification_center-Swift.h>
-
-@implementation NotificationCenterPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftNotificationCenterPlugin registerWithRegistrar:registrar];
-}
-@end
